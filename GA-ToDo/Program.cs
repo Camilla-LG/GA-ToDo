cd2e7241cbff8fbbae5334a7e987fe7bcdde7674
camillalo1 <camillalo@getacademy.no>
2023-12-07 00:12:27 +0100

    Add project files.

---
+++ GA-ToDo/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace GA_ToDo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var taskProgram = new TaskProgram();
+            taskProgram.Run();
+        }
+    }
+
+    internal class TaskProgram
+    { 
+        Tasks TasksInstance = new Tasks("", "");
+        bool RunProgram = true;
+
+        public void Run()
+        { 
+            while (RunProgram == true)
+            {
+                MainMenu();
+               
+                var UserChoice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (UserChoice)
+                {
+                    case '1':
+                        TasksInstance.AddTask("","");
+                        break;
+                    case '2':
+                        TasksInstance.ShowTasks("", "");
+                        break;
+                    case '3':
+                        TasksInstance.DeleteTask("", "");
+                        break;
+                    case '4':
+                        Console.WriteLine("Ha en fin dag!");
+                        RunProgram = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void MainMenu()
+        {
+            Console.WriteLine("*** To Do Console App ***");
+            Console.WriteLine();
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("1. Legg til oppgave.");
+            Console.WriteLine("2. Vis liste med oppgaver.");
+            Console.WriteLine("3. Slett oppgave fra listen.");
+            Console.WriteLine("4. Exit.");
+        }
+    }
+}
---
+++ GA-ToDo/Tasks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_ToDo
+{
+    internal class Tasks
+    {
+        public List<Tasks> ListOfTasks = new List<Tasks>();
+        public string Task {  get; set; }
+        public string Deadline { get; set; }
+
+        public Tasks(string task, string deadline) 
+        {
+            Task = task;
+            Deadline = deadline;
+        }
+
+        public void AddTask(string task, string deadline)
+        {
+            Console.WriteLine("Skriv inn oppgaven som må gjøres.");
+            task = Console.ReadLine();
+            Console.WriteLine("Skriv inn frist for når oppgaven må være utført.");
+            deadline = Console.ReadLine();
+            ListOfTasks.Add(new Tasks(task, deadline));
+            Console.WriteLine("Oppgave og frist er lagt til listen.");
+        }
+
+        public void ShowTasks(string task, string deadline)
+        {
+            foreach(var x in ListOfTasks) 
+            {
+                Console.WriteLine($" *  {this.Task}, frist: {this.Deadline}");
+            }
+            Console.WriteLine();
+        }
+
+        public void DeleteTask(string task, string deadline)
+        {
+
+        }
+    }
+}
